daf73323672d6120d2a278711884326f92bf0724
Emanuel <20240230@ricaldone.edu.sv>
2025-06-02 12:09:59 -0600

    Funcion para agregar pendiente

--- js/script.js
+++ js/script.js
@@ -30,3 +30,60 @@ function mostrarDatos(datos){
 }
 
 obtenerPersonas();
+
+
+
+
+//Agregar un nuevo registro//
+const modal = document.getElementById("modal-agregar");//cuadro de dialogo
+const btnAgregar = document.getElementById("btnAbrirModal");//+ para abrir
+const btnCerrar = document.getElementById("btnCerrarModal");//x para cerrar
+
+btnAgregar.addEventListener("click", () => {
+    modal.showModal();//Abrir el modal al hacer clic
+});
+
+btnCerrar.addEventListener("click", () => {
+    modal.close();//Cerrar modal
+});
+
+
+//Agregar nuevo integrante desde el formulario
+document.getElementById("frmAgregar").addEventListener("submit", async e => {
+    e.preventDefault(); // e representa a submit - evita que el formilario se envie de golpe
+
+    //capturar los valores del formulario
+    const nombre = getElementById("nombre").value.trim();
+    const apellido = getElementById("apellido").value.trim();
+    const email = getElementById("email").value.trim();
+    const edad = getElementById("edad").value.trim();
+
+    //validacion basica
+    if(!nombre || !apellido || !email || !edad){
+        alert("complete todos los campos");
+        return;
+    }
+
+    //Llamamos a la API
+    const respuesta = await fetch(API_URL, {
+        method: "POST", 
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({nombre, apellido, email, edad})
+    });
+
+    if(respuesta.ok){
+        alert("El registro fue agregado correctamente");
+
+        //Limpiar el formulario y cerrar el model
+
+        document.getElementById("frmAgregar").reset();
+
+        modal.close();
+
+        //Recargar la tabla
+        obtenerPersonas();
+    }
+    else{
+        alert("Ocurrio un error al agregar");
+    }
+});
